bb4f90ea67e9c76fc522a04dc0e69edd6b28f25b
Elie Azzi <elie.a@eurisko.net>
2024-08-20 23:15:01 +0300

    fix: fixed chart x length

--- src/app/pool/components/chart/Chart.tsx
+++ src/app/pool/components/chart/Chart.tsx
@@ -3,55 +3,6 @@ import { Card, CardBody } from "@nextui-org/react";
 import React from "react";
 import Chart from "react-google-charts";
 
-export const options = {
-  colors: ["white"],
-  backgroundColor: "transparent",
-  vAxis: {
-    title: "APR (percent)",
-    viewWindow: {
-      min: 0,
-      max: 100, // Adjust this to provide some padding above the highest point
-    },
-    titleTextStyle: {
-      color: "#ffffff", // Change title text color to white
-    },
-    textStyle: {
-      color: "#ffffff", // Change axis text color to white
-    },
-    gridlines: {
-      count: 3, // Ensure there are enough gridlines for padding
-    },
-    ticks: [50, 100],
-  },
-  hAxis: {
-    title: "Epoch",
-    viewWindow: {
-      min: 0,
-      max: 4, // Adjust this to provide some padding above the highest point
-    },
-    gridlines: {
-      count: 4, // Ensure there are enough gridlines for padding
-    },
-    titleTextStyle: {
-      color: "#ffffff", // Change title text color to white
-    },
-    textStyle: {
-      color: "#ffffff", // Change axis text color to white
-    },
-  },
-  chartArea: {
-    left: 40, // Adjust this to add padding on the left
-    top: 30, // Adjust this to add padding on the top
-    right: 30, // Adjust this to add padding on the right
-    bottom: 40, // Adjust this to add padding on the bottom
-    width: "100%", // Adjust this to control the chart area width
-    height: "100%", // Adjust this to control the chart area height
-  },
-  pointSize: 5, // Size of the points
-  pointsVisible: true,
-  legend: "none",
-};
-
 const LineChart = ({
   array,
 }: {
@@ -64,6 +15,55 @@ const LineChart = ({
       }[]
     | undefined;
 }) => {
+  const options = {
+    colors: ["white"],
+    backgroundColor: "transparent",
+    vAxis: {
+      title: "APR (percent)",
+      viewWindow: {
+        min: 0,
+        max: 100, // Adjust this to provide some padding above the highest point
+      },
+      titleTextStyle: {
+        color: "#ffffff", // Change title text color to white
+      },
+      textStyle: {
+        color: "#ffffff", // Change axis text color to white
+      },
+      gridlines: {
+        count: 3, // Ensure there are enough gridlines for padding
+      },
+      ticks: [50, 100],
+    },
+    hAxis: {
+      title: "Epoch",
+      viewWindow: {
+        min: 0,
+        max: (array?.length ?? 0) + 2, // Adjust this to provide some padding above the highest point
+      },
+      gridlines: {
+        count: 4, // Ensure there are enough gridlines for padding
+      },
+      titleTextStyle: {
+        color: "#ffffff", // Change title text color to white
+      },
+      textStyle: {
+        color: "#ffffff", // Change axis text color to white
+      },
+    },
+    chartArea: {
+      left: 40, // Adjust this to add padding on the left
+      top: 30, // Adjust this to add padding on the top
+      right: 30, // Adjust this to add padding on the right
+      bottom: 40, // Adjust this to add padding on the bottom
+      width: "100%", // Adjust this to control the chart area width
+      height: "100%", // Adjust this to control the chart area height
+    },
+    pointSize: 5, // Size of the points
+    pointsVisible: true,
+    legend: "none",
+  };
+
   const data: (string | number)[][] = [["Epoch", "APR"]];
 
   array?.map((item, index) => {
